a142b080bb81d5563b20f1325e0c9e436c82cfd4
Filip Nestorovic <filipnestorovic@yahoo.com>
2025-12-11 10:30:51 +0100

    Second commit 3

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    Route::view('/nailrepair', 'landers.nailrepair');
+});
 
 Route::post('/order', [App\Http\Controllers\OrderController::class, 'store'])->name('order.create');
 
