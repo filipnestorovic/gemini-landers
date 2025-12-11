38a58388c4d4c107bc97d4f73ad4d25f53a875d0
Filip Nestorovic <filipnestorovic@yahoo.com>
2025-12-11 10:38:22 +0100

    Route / change 1

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    Route::view('/nailrepair', 'landers.nailrepair');
-});
 
 Route::post('/order', [App\Http\Controllers\OrderController::class, 'store'])->name('order.create');
 
+Route::view('/', 'landers.nailrepair');
 Route::view('/nailrepair', 'landers.nailrepair');
